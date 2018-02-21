75dff42729249e89cf5ba5caa62ecee9f0fbf3e2
Eric <Eric@Jennifer-Johns-MacBook-Pro.local>
2018-02-21 12:56:32 -0800

    Added LandingPage class and assets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import './styles/App.css';
-import Header from './Header';
-import Footer from './Footer';
+import LandingPage from './LandingPage';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <LandingPage />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Footer />
       </div>
     );
   }
---
+++ src/LandingPage.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
