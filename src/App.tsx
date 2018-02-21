50c1bc83bc70e5b2d3dc4bfbb24b22535e5621b5
Eric <Eric@Jennifer-Johns-MacBook-Pro.local>
2018-02-21 11:18:28 -0800

    Basic footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './styles/App.css';
 import Header from './Header';
+import Footer from './Footer';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Footer />
       </div>
     );
   }
