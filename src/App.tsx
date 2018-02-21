07aa7a8baab73e96a5202935a49a2f04974d3aeb
serpient <littlegangwolf@gmail.com>
2018-02-20 20:46:15 -0800

    added links to variables scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import './styles/App.css';
-
-const logo = require('./logo.svg');
+import Header from './Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
