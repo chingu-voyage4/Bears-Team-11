878a5f923f77d34b78889dffd5828ebdf91ad986
Eric <ericmiller777@yahoo.com>
2018-02-22 00:33:22 -0800

    Minor Change to ReadyToTry class.

--- src/ReadyToTry.tsx
+++ src/ReadyToTry.tsx
@@ -4,7 +4,7 @@ import './styles/ReadyToTry.css';
 class ReadyToTry extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ready-to-try-container">
          <h1 className="ready-to-try-text">Ready to try Project Match?</h1>
          <button className="ready-to-try-button">SIGN UP FOR FREE</button>
       </div>
