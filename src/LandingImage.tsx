5e717dcf351ec51f70739933e3b70c9f90ca878b
Eric <ericmiller777@yahoo.com>
2018-02-21 18:54:17 -0800

    Test image size, and text.

--- src/LandingImage.tsx
+++ src/LandingImage.tsx
@@ -4,8 +4,9 @@ import './styles/LandingImage.css';
 class LandingImage extends React.Component {
   render() {
     return (
-      <div className="mainImage">
-          <img src="/assets/Landing_Image-02.png" />
+      <div className="landing-image-container">
+          <h1 className="header">Get Project Matched!</h1>
+          <img className="image" src={require('./assets/Landing_Image-02.png')} />
       </div>
     );
   }
