970c3edd1dbc0c0819f6c542062fe4273e317ab0
Eric <ericmiller777@yahoo.com>
2018-02-21 17:58:08 -0800

    Added landingimage, and projectmatch classes.

---
+++ src/ProjectMatch.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class ProjectMatch extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>PlaceHolder</h1>
+      </div>
+    );
+  }
+}
+
+export default ProjectMatch;
