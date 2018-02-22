eb94ccc20fad2caf45870e8ba8e36a5d9af00db1
serpient <littlegangwolf@gmail.com>
2018-02-21 19:36:17 -0800

    attempt 1 on creating blue sky and green grass

--- src/LandingImage.tsx
+++ src/LandingImage.tsx
@@ -5,8 +5,10 @@ class LandingImage extends React.Component {
   render() {
     return (
       <div className="landing-image-container">
+        <div className="blue-sky green-grass">
           <h1 className="header">Get Project Matched!</h1>
           <img className="image" src={require('./assets/Landing_Image-02.png')} />
+        </div>
       </div>
     );
   }
