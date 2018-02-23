70f34238528f5df9c3ebbf7042465f56efe1cce6
serpient <littlegangwolf@gmail.com>
2018-02-23 15:14:54 -0800

    adjusted prioject feature styling

--- src/Landing Page/ProjectFeatures.tsx
+++ src/Landing Page/ProjectFeatures.tsx
@@ -6,25 +6,25 @@ class ProjectFeatures extends React.Component {
     return (
       <div>
         <div className="project-match-container">
-          <div className="project-match-text">
-            <h1>PROJECT MATCH</h1>
-            <h3>Programmers and designers use Project Match
-               to find teammates for budding ideas. Project Match
-                will help find you projects and then provide team
-                 collaboration tools to support mockup reviews.</h3>
-          </div>
+          <h1 className="project-feature-heading">PROJECT MATCH</h1>
+          <h3 className="project-feature-text">
+            Programmers and designers use Project Match
+            to find teammates for budding ideas. Project Match
+            will help find you projects and then provide team
+            collaboration tools to support mockup reviews.
+            </h3>
           <div className="project-feature-image-div">
             <img className="project-feature-imac" src={require('../assets/proj_imac.png')} />
           </div>
         </div>
         <div className="team-collaboration-container">
-          <div className="project-feature-text">
-            <h1>TEAM COLLABORATION</h1>
-            <h3>Programmers and designers use Project Match
-                 to find teammates for budding ideas. Project
-                  Match will help find you projects and then provide team
-                collaboration tools to support mockup reviews.</h3>
-          </div>
+          <h1 className="project-feature-heading">TEAM COLLABORATION</h1>
+          <h3 className="project-feature-text">
+            Programmers and designers use Project Match
+            to find teammates for budding ideas. Project
+            Match will help find you projects and then provide team
+            collaboration tools to support mockup reviews.
+          </h3>
           <div className="project-feature-image-div">
             <img className="project-feature-imac" src={require('../assets/proj_imac.png')} />
           </div>
