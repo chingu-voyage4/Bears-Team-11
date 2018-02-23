b21de492f52d2d462486afcb72424090cc062229
serpient <littlegangwolf@gmail.com>
2018-02-23 14:57:57 -0800

    CSS changes for "ready to try" component in landing page; adjusted projects component border

--- src/Landing Page/RecentProjects.tsx
+++ src/Landing Page/RecentProjects.tsx
@@ -6,9 +6,11 @@ class RecentProjects extends React.Component {
   render() {
     return (
       <div>
+          <hr className="horizontal-line" />
           <h1 className="recent-projects-header">Recent Projects</h1>
           <Projects count={6}/>
           <button className="explore-projects-button">Explore Projects</button>
+          <hr className="horizontal-line" />
       </div>
     );
   }
