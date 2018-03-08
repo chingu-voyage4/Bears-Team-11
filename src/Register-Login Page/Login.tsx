eb5327519f6601f800186840de22635b77a933cb
Tony Luo <luo.tony37@gmail.com>
2018-03-08 12:41:18 -0800

    Connect redux to Login component

--- src/Register-Login Page/Login.tsx
+++ src/Register-Login Page/Login.tsx
@@ -1,116 +1,97 @@
 import * as React from 'react';
 import '../styles/Register-Login.css';
-import { PassedProps, State, Props } from '../types/Login.d';
+import { State } from '../types/Login.d';
+import { Props } from '../types/Redux.d';
+import { connect } from 'react-redux';
+import { login } from '../actions/userActions';
 
-const fetch = require('isomorphic-fetch');
+class Login extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      Email: '',
+      Password: ''
+    };
+  }
+  handleEmail = (e: React.FormEvent<HTMLInputElement>): void => {
+    this.setState({
+      Email: e.currentTarget.value
+    });
+  };
 
-class Login extends React.Component<PassedProps, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            Email: '',
-            Password: ''
-        };
-    }
-    handleEmail = (e: React.FormEvent<HTMLInputElement>): void => {
-        this.setState({
-            Email: e.currentTarget.value
-        });
-    }
+  handlePassword = (e: React.FormEvent<HTMLInputElement>): void => {
+    this.setState({
+      Password: e.currentTarget.value
+    });
+  };
 
-    handlePassword = (e: React.FormEvent<HTMLInputElement>): void => {
-        this.setState({
-            Password: e.currentTarget.value
-        });
-    }
+  handleSubmit = (e: React.FormEvent<HTMLButtonElement>): void => {
+    this.props.login(this.state.Email, this.state.Password);
+  };
+  render() {
+    return (
+      <div className="popupScreen">
+        <br />
 
-    handleSubmit = (e: React.FormEvent<HTMLButtonElement>): void => {
+        <div className="logo">project match</div>
 
-        var apiBaseUrl = 'http://localhost:8080/api/v1/user/login';
+        <br />
 
-        var payload = {
-            email: this.state.Email,
-            password: this.state.Password
-        };
+        <img
+          className="extAuthIcon"
+          src={require('../assets/google icon.png')}
+        />
+        <button className="extAuthBtn">Sign in with Google</button>
 
-        let data = {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(payload)
-        };
+        <br />
 
-        fetch(apiBaseUrl, data)
-            /* tslint:disable-next-line */
-            .then(function (response: any) {
-                if (response.status === 200) {
-                    alert('User Logged In');
-                } else if (response.status === 204) {
-                    alert('Username password do not match');
-                } else {
-                    alert('Status Code: ' + response.status + ' - ' + response.statusText);
-                }
-            })
-            /* tslint:disable-next-line */
-            .catch(function (error: any) {
-                alert(error);
-            });
-    }
-    render() {
-        return (
-            <div className="popupScreen">
-                <br />
+        <img
+          className="extAuthIcon"
+          src={require('../assets/facebook icon.png')}
+        />
+        <button className="extAuthBtn">Sign in with Facebook</button>
 
-                <div className="logo">project match</div>
+        <br />
 
-                <br />
+        <img
+          className="extAuthIcon"
+          src={require('../assets/chingu icon.png')}
+        />
+        <button className="extAuthBtn">Sign in with Chingu</button>
 
-                <img className="extAuthIcon" src={require('../assets/google icon.png')} />
-                <button className="extAuthBtn">
-                    Sign in with Google
-                </button>
+        <hr className="horizontalDivider" />
 
-                <br />
-
-                <img className="extAuthIcon" src={require('../assets/facebook icon.png')} />
-                <button className="extAuthBtn">
-                    Sign in with Facebook
-                </button>
-
-                <br />
-
-                <img className="extAuthIcon" src={require('../assets/chingu icon.png')} />
-                <button className="extAuthBtn">
-                    Sign in with Chingu
-                </button>
-
-                <hr className="horizontalDivider" />
-
-                <form>
-                    <label className="form-label">Your Email</label>
-                    <input
-                        className="emailDiv"
-                        type="email"
-                        name="email"
-                        value={this.state.Email}
-                        onChange={e => this.handleEmail(e)}
-                        placeholder="Email"
-                    />
-                    <br />
-                    <label className="form-label">Password</label>
-                    <input
-                        className="passwordDiv"
-                        type="password"
-                        name="password"
-                        value={this.state.Password}
-                        onChange={e => this.handlePassword(e)}
-                        placeholder="Password"
-                    />
-                    <br />
-                    <input type="submit" className="signUpBtn" value="Submit" onClick={e => this.handleSubmit(e)} />
-                </form>
-            </div>
-        );
-    }
+        <form>
+          <label className="form-label">Your Email</label>
+          <input
+            className="emailDiv"
+            type="email"
+            name="email"
+            value={this.state.Email}
+            onChange={e => this.handleEmail(e)}
+            placeholder="Email"
+          />
+          <br />
+          <label className="form-label">Password</label>
+          <input
+            className="passwordDiv"
+            type="password"
+            name="password"
+            value={this.state.Password}
+            onChange={e => this.handlePassword(e)}
+            placeholder="Password"
+          />
+          <br />
+          <input
+            type="submit"
+            className="signUpBtn"
+            value="Submit"
+            onClick={e => this.handleSubmit(e)}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Login;
+export default connect(null, { login })(Login);
