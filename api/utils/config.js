8be094cf9b6e8f83feebcbea8c9b9121b9cff81b
serpient <littlegangwolf@gmail.com>
2018-03-14 14:03:33 -0700

    resolved merge conflicts in API folder

--- api/utils/config.js
+++ api/utils/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const sessionSecret = 'super secret'; // make sure to change in produciton
 const db = 'Some Db String'
 module.exports = {
@@ -8,14 +7,3 @@ module.exports = {
         'mlab'  : 'mongodb://bt11:b11@ds019480.mlab.com:19480/pmatch'
     }
 };
-=======
-const sessionSecret = 'super secret'; // make sure to change in produciton
-const db = 'Some Db String'
-module.exports = {
-    sessionSecret,
-    db:{
-        'local' : 'mongodb://localhost/team',
-        'mlab'  : 'mongodb://bt11:b11@ds019480.mlab.com:19480/pmatch'
-    }
-};
->>>>>>> 4ed8682411a815ae7d3720712f4a324b05281132
