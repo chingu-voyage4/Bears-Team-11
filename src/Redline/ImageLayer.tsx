cb38780e4565273baff85d7121f1bed818cb0029
Tony Luo <luo.tony37@gmail.com>
2018-04-10 15:16:50 -0700

    Add ImageLayer component

---
+++ src/Redline/ImageLayer.tsx
@@ -0,0 +1,18 @@
+/* tslint: disable */
+import * as React from 'react';
+
+class ImageLayer extends React.Component {
+  render() {
+    return (
+      <div className="redline-image-container">
+        <img
+          className="redline-image"
+          src="http://www.relativelyinteresting.com/wp-content/uploads/2017/06/mini-kaiju-size-chart.jpg"
+          alt=""
+        />
+      </div>
+    );
+  }
+}
+
+export default ImageLayer;
