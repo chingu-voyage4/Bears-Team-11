7bdd633a0b57a2a5875bd1cfe102e689390649de
Tony Luo <luo.tony37@gmail.com>
2018-04-12 11:59:22 -0700

    Adjust comment box styles to support multiple comments

--- src/Redline/CommentBox.tsx
+++ src/Redline/CommentBox.tsx
@@ -10,10 +10,41 @@ class CommentBox extends React.Component<{
   render() {
     return (
       <div className="comment-box">
-        <div className="comment-box__menu">
-          {/* <i className="far fa-times-circle" /> */}
+        {/* need a menu ? */}
+        {/* <div className="comment-box__menu">
+          <i className="far fa-times-circle" />
+        </div> */}
+        <div className="comment-box__comment">
+          <img
+            className="comment-box__profile_picture"
+            src="https://stroops.com/wp-content/uploads/2016/11/placeholder-profile-male-500x500.png"
+            alt=""
+          />
+          <div>
+            <p className="comment-box__name">
+              natapot <span className="comment-box__timestamp">8:00am</span>
+            </p>
+            <p className="comment-box__body">
+              Dislike Kaiju, please make it more cool.
+            </p>
+          </div>
         </div>
-        <div className="comment-box__header">
+        <div className="comment-box__comment">
+          <img
+            className="comment-box__profile_picture"
+            src="https://stroops.com/wp-content/uploads/2016/11/placeholder-profile-male-500x500.png"
+            alt=""
+          />
+          <div>
+            <p className="comment-box__name">
+              natapot <span className="comment-box__timestamp">8:00am</span>
+            </p>
+            <p className="comment-box__body">
+              Dislike Kaiju, please make it more cool.
+            </p>
+          </div>
+        </div>
+        <div className="comment-box__comment">
           <img
             className="comment-box__profile_picture"
             src="https://stroops.com/wp-content/uploads/2016/11/placeholder-profile-male-500x500.png"
