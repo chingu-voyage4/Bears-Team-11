6fad77a6361f0c9715b6942dec20f42f8d3237e0
Tony Luo <luo.tony37@gmail.com>
2018-04-26 21:28:29 -0700

    Remove comments

--- src/GoogleSignIn/index.tsx
+++ src/GoogleSignIn/index.tsx
@@ -25,22 +25,7 @@ export class GoogleSignIn extends React.Component<GoogleProps> {
   }
 
   onSignIn = (googleUser: any): void => {
-    // let auth2 = window.gapi.auth2.init({
-    //   client_id:
-    //     '634604962663-247j6obodp1clln54de1469euufj6vdj.apps.googleusercontent.com'
-    // });
-
-    // var profile = auth2.currentUser.get().getBasicProfile();
-    // console.log('ID: ' + profile.getId());
-    // console.log('Full Name: ' + profile.getName());
-    // console.log('Given Name: ' + profile.getGivenName());
-    // console.log('Family Name: ' + profile.getFamilyName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail());
-
-    // Pass ID token to backend and verify with serverside secret
     var id_token = googleUser.getAuthResponse().id_token;
-    // console.log('ID Token: ' + id_token);
     this.props.googleLogin(id_token);
   };
 
