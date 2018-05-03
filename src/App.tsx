73d521e55254df45ddc55bf58d443a3eeca9a61d
serpient <littlegangwolf@gmail.com>
2018-05-03 12:12:52 -0700

    ErrorPage implemented

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import PublicProfile from './PublicProfile/PublicProfile';
 import Redline from './Redline';
 import ProjectPortalPage from './ProjectPortalPage';
 import { HYDRATE_USER } from './actions/actionTypes';
+import ErrorPage from './404Page/ErrorPage';
 
 const store = createStore(
   rootReducer,
@@ -76,6 +77,7 @@ class App extends React.Component<{}, { reduxManualTest: boolean }> {
                 path="/user/profile"
                 component={PublicProfile}
               />
+              <Route exact={true} path="/404" component={ErrorPage} />
             </Switch>
           </Router>
         </div>
