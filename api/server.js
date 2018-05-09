15bf080bee0e880ee657c2d0bf4ded0ed17a85a3
serpient <littlegangwolf@gmail.com>
2018-05-09 02:21:10 -0700

    master branch set to origin manually until production mode can be built

--- api/server.js
+++ api/server.js
@@ -45,7 +45,7 @@ mongoose.connect(config.db.mlab, {
 
 app.use(
   cors({
-    origin: config.host.name,
+    origin: 'https://projectmatch.me',
     credentials: true,
     preflightContinue: true,
     optionsSuccessStatus: 200
